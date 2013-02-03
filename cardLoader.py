6440741311379a70b8c885e567fb5dd3a2d6ab36
Adam Babcock <github@mrhen.com>
2013-02-03 11:22:02 -0600

    Filter changes and use new card output from Fansite * Add heath and uncommon filter * Add exceptions to filters * Fansite card download now has ids

--- cardLoader.py
+++ cardLoader.py
@@ -2,7 +2,7 @@ import xml.sax.handler
 from collections import namedtuple
 from collections import Counter
 
-CardData = namedtuple("CardData", ['id', 'name', 'rarity', 'unique', 'base_card'])
+CardData = namedtuple("CardData", ['id', 'name', 'health', 'rarity', 'unique', 'base_card'])
 
 class CardHandler(xml.sax.handler.ContentHandler):
   def __init__(self):
@@ -17,12 +17,13 @@ class CardHandler(xml.sax.handler.ContentHandler):
     #print(self.subs.keys())
     if element == "unit":
         self.inUnit = 1
+        self.subs["id"] = ""
+        self.subs["name"] = ""
+        self.subs["health"] = "0"
+        self.subs["cost"] = "-1"
         self.subs["rarity"] = "0"
         self.subs["unique"] = "0"
-        self.subs["id"] = ""
         self.subs["set"] = ""
-        self.subs["name"] = ""
-        self.subs["cost"] = "-1"
         self.subs["base_card"] = "0"
     elif (element in self.subs.keys()):
       self.inSubelement = 1
@@ -41,6 +42,7 @@ class CardHandler(xml.sax.handler.ContentHandler):
         self.ids.append(int(self.subs["id"]))
         self.cards.append(CardData(int(self.subs["id"]),
             self.subs["name"],
+            int(self.subs["health"]),
             int(self.subs["rarity"]),
             int(self.subs["unique"]),
             int(self.subs["base_card"])))
@@ -82,16 +84,20 @@ def loadCardsFromNameFile(file, cardData):
     for line in f:
         line = line.strip()
         if(len(line) == 0 or line[0] == "#"): continue
-        name, count = line.split('(')
-        count = count.strip(')')
-        cardCounts[name] = int(count)
-        #print("found '" + name + "'\twith count " + count)
+
+        id = line[line.find("[") + 1:line.find("]")]
+        count = line[line.find("(") + 1:line.find(")")]
+        
+        cardCounts[int(id)] = int(count)
+        #print("found '" + id + "'\twith count " + count)
         #names.append(name) #TODO support card counts as well
     f.close()
 
-    idCounts = [[card.id, cardCounts[card.name]] for card in cardData if (cardCounts[card.name] > 0 and card.base_card == 0)]
-    counts = Counter(dict(idCounts))
-    return counts
+    #idCounts = [[card.id, cardCounts[card.name]] for card in cardData if (cardCounts[card.id] > 0 and card.base_card == 0)]
+    #counts = Counter(dict(idCounts))
+    #return counts
+    #print(cardCounts)
+    return cardCounts
 
 def loadCardsWithArgs(args):
     cardsFile = args.cardsFile
@@ -106,7 +112,52 @@ def loadCardsWithArgs(args):
 
     return cards
 
-def getIdsFromCardData(cardData, filter = [], ignoreActions = True, ignoreCommons = True):
+def getNotBadExceptions():
+    exceptions = set()
+
+#Acid Athenor
+    exceptions.add(2012)    # Asylum
+    exceptions.add(752) #Atelier
+    exceptions.add(2032)    #Blood Wall
+    exceptions.add(2101)    #Boot Camp
+    exceptions.add(1121)    #Daizon
+    exceptions.add(769) #Egg Infector
+    exceptions.add(2118)    #Enclave Warp Gate
+    exceptions.add(2065)    #Fortified Cannons
+    exceptions.add(907) #Gene Reader
+    exceptions.add(320) #Gruesome Crawler
+    exceptions.add(442) #Heli-Duster
+    exceptions.add(37)  #Hunter
+    exceptions.add(1020)    #Hydraulis
+    exceptions.add(238) #Irradiated Infantry
+    exceptions.add(720) #Meteor
+    exceptions.add(2034)    #Mirror Wall
+    exceptions.add(949) #Mizar VIII
+    exceptions.add(888) #Nephilim Knight
+    exceptions.add(626) #Neverender
+    exceptions.add(2070)    #Offshore Platform
+    exceptions.add(457) #Outfitted Scow
+    exceptions.add(126) #Pathrazer
+    exceptions.add(472)    #Patrol Cruiser
+    exceptions.add(1154)    #Patriarch
+    exceptions.add(783) #Phantasm
+#Phaseid
+    exceptions.add(30)  #Predator
+    exceptions.add(2090)    #Prism
+    exceptions.add(444) #Reclamax
+    exceptions.add(1035)    #Ryoko
+    exceptions.add(452) #Shaded Hollow
+    exceptions.add(2100)    #Shrapnel Engine
+    exceptions.add(2129)    #Shrine of Hope
+    exceptions.add(154) #Speculus
+    exceptions.add(261) #Stealthy Niaq
+    exceptions.add(534) #Subterfuge
+    exceptions.add(500) #Sundering Ogre
+    exceptions.add(103) #Tiamat
+    exceptions.add(245) #Toxic Cannon
+    exceptions.add(865) #Withersnap
+
+def getIdsFromCardData(cardData, filter = [], ignoreActions = True, ignoreBad = True):
     baseCardData = [card for card in cardData if card.base_card == 0]
 
     cardIds = set([card.id for card in baseCardData])
@@ -116,13 +167,18 @@ def getIdsFromCardData(cardData, filter = [], ignoreActions = True, ignoreCommon
     if(len(filter) > 0):
         cardIds = cardIds & filter
 
-    commanderIds = set([id for id in cardIds if (id >= 1000 and id < 2000)])
-
     # ignore commons for assault and structure cards
-    if(ignoreCommons):
-        commonIds = set([card.id for card in baseCardData if card.rarity == 1])
-        cardIds = cardIds - commonIds
+    if(ignoreBad):
+        commonIds = set([card.id for card in cardData if card.rarity == 1])
+        uncommonIds = set([card.id for card in cardData if card.rarity == 2])
+        lowHealthIds = set([card.id for card in cardData if card.health <= 2])
 
+        badIds = commonIds | uncommonIds | lowHealthIds
+        notBadIds = getNotBadExceptions()
+        badIds = badIds - notBadIds
+        cardIds = cardIds - badIds
+
+    commanderIds = set([id for id in cardIds if (id >= 1000 and id < 2000)])
     assaultIds = set([id for id in cardIds if id < 1000])
     structureIds = set([id for id in cardIds if (id >= 2000 and id < 3000)])
     playedIds = assaultIds | structureIds
